7e10078a50906cf7cca9ffacc167627902cc1a95
eu <ekazyulin@gmail.com>
2025-03-03 03:03:55 +0300

    added multi-stage build

--- dockerfile
+++ dockerfile
@@ -1,9 +1,16 @@
-FROM golang:1.24-alpine
+FROM golang:1.24-alpine AS build
 
 WORKDIR /app
 
 COPY main.go .
 
 RUN go build -o docker-go-hello main.go
+
+
+FROM alpine:3.15
+
+WORKDIR /app
+
+COPY --from=build  /app/docker-go-hello .
 
 CMD [ "./docker-go-hello" ]
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Hello from Docker!")
 		fmt.Fprintln(w, "Now alpine version!")
+		fmt.Fprintln(w, "v1.2, absolutely light-weight cause of multi-stage build!")
 	})
 
 	http.ListenAndServe(":12345", nil)
